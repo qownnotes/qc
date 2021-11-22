67c6dd03a5c0edf47fc08a5b4aa2db2863ba48a0
Patrizio Bekerle <patrizio@bekerle.com>
2021-11-22 21:20:10 +0100

    Add build and test config

---
+++ scripts/package.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+set -x
+
+VERSION=$(grep "version = " cmd/root.go | sed -E 's/.*"(.+)"$/\1/')
+REPO="qc"
+
+rm -rf ./out/
+gox --osarch "windows/386 windows/amd64 darwin/386 darwin/amd64 linux/386 linux/amd64" -output="./out/${REPO}_${VERSION}_{{.OS}}_{{.Arch}}/{{.Dir}}"
+
+rm -rf ./pkg/
+mkdir ./pkg
+
+for PLATFORM in $(find ./out -mindepth 1 -maxdepth 1 -type d); do
+        PLATFORM_NAME=$(basename ${PLATFORM})
+
+        pushd ${PLATFORM}
+        cp -r ../../misc ./
+        zip -r ../../pkg/${PLATFORM_NAME}.zip ./*
+        popd
+done
