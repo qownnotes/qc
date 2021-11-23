3516948f10afcecfa4d066e6ab8558c58be5dfe4
Patrizio Bekerle <patrizio@bekerle.com>
2021-11-23 10:19:07 +0100

    Cache snippets in case QOwnNotes is not running

--- websocket/websocket.go
+++ websocket/websocket.go
@@ -2,9 +2,13 @@ package websocket
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/qownnotes/qc/config"
 	"github.com/qownnotes/qc/entity"
 	"log"
+	"os"
+	"path/filepath"
 	"strconv"
 
 	// "net/http"
@@ -13,6 +17,10 @@ import (
 	"time"
 
 	"github.com/gorilla/websocket"
+)
+
+var (
+	snippetCacheFile string
 )
 
 type Message struct {
@@ -50,33 +58,43 @@ func FetchSnippetsData() []entity.SnippetInfo {
 	u := url.URL{Scheme: "ws", Host: "127.0.0.1:" + strconv.Itoa(config.Conf.QOwnNotes.WebSocketPort)}
 	//log.Printf("Connecting to QOwnNotes on %s", u.String())
 
+	initSnippetCacheFile()
+	var snippetData []byte = nil
+
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		log.Fatal("Couldn't connect to QOwnNotes websocket, did you enable the socket server? Error: ", err)
-	}
-	defer c.Close()
+		snippetData = readSnippetCacheFile()
 
-	message := Message{
-		Token: config.Conf.QOwnNotes.Token,
-		Type:  "getCommandSnippets",
-	}
-
-	m, err := json.Marshal(message)
-
-	err = c.WriteMessage(websocket.TextMessage, m)
-	if err != nil {
-		log.Fatal("Couldn't send command to QOwnNotes: ", err)
+		if snippetData == nil {
+			log.Fatal("Couldn't connect to QOwnNotes websocket, did you enable the socket server? Error: ", err)
+        } else {
+			log.Println("Couldn't connect to QOwnNotes websocket, but found cached data in " + snippetCacheFile)
+		}
 	}
 
-	_, msg, err := c.ReadMessage()
-	if err != nil {
-		log.Fatalf("Couldn't read message from QOwnNotes: %v", err)
-	}
+	if snippetData == nil {
+		defer c.Close()
 
-	// log.Printf("msg: %v", msg)
+		message := Message{
+			Token: config.Conf.QOwnNotes.Token,
+			Type:  "getCommandSnippets",
+		}
+
+		m, err := json.Marshal(message)
+
+		err = c.WriteMessage(websocket.TextMessage, m)
+		if err != nil {
+			log.Fatal("Couldn't send command to QOwnNotes: ", err)
+		}
+
+		_, snippetData, err = c.ReadMessage()
+		if err != nil {
+			log.Fatalf("Couldn't read message from QOwnNotes: %v", err)
+		}
+	}
 
 	var resultMessage ResultMessage
-	err = json.Unmarshal(msg, &resultMessage)
+	err = json.Unmarshal(snippetData, &resultMessage)
 	if err != nil {
 		log.Fatalf("Couldn't interpret message from QOwnNotes: %v", err)
 	}
@@ -85,6 +103,8 @@ func FetchSnippetsData() []entity.SnippetInfo {
 	case "tokenQuery":
 		log.Fatal("Please execute \"qc configure\" and configure your token from QOwnNotes!")
 	case "commandSnippets":
+		writeSnippetCacheFile(snippetData)
+
 		//log.Printf("CommandSnippets: %v", resultMessage.CommandSnippets)
 		return resultMessage.CommandSnippets
 	default:
@@ -92,6 +112,40 @@ func FetchSnippetsData() []entity.SnippetInfo {
 	}
 
 	return []entity.SnippetInfo{}
+}
+
+func initSnippetCacheFile() {
+	if snippetCacheFile == "" {
+		dir, err := config.GetDefaultConfigDir()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+			os.Exit(1)
+		}
+
+		snippetCacheFile = filepath.Join(dir, "snippets.cache")
+	}
+}
+
+func writeSnippetCacheFile(data []byte) {
+	if err := os.WriteFile(snippetCacheFile, data, 0666); err != nil {
+		log.Fatal("Could not write snippet cache file: ", err)
+	}
+}
+
+func readSnippetCacheFile() []byte {
+	_, err := os.Stat(snippetCacheFile)
+
+	if errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+
+	data, err := os.ReadFile(snippetCacheFile)
+
+	if err != nil {
+		log.Fatal("Could not read snippet cache file: ", err)
+	}
+
+	return data
 }
 
 //
