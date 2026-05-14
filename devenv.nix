805f11e6199cdc39db72d88030b29f4db34f76fa
Patrizio Bekerle <patrizio@bekerle.com>
2026-05-14 14:51:33 +0200

    devenv: add shared go env
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- devenv.nix
+++ devenv.nix
@@ -1,22 +1,7 @@
 {
-  # https://devenv.sh/languages/
-  languages.go.enable = true;
-
   enterShell = ''
     echo "🛠️ qc dev shell"
-    echo "🐹 Go version: $(go version | cut -d' ' -f3 | sed 's/^go//')"
   '';
-
-  # https://devenv.sh/git-hooks/
-  git-hooks = {
-    hooks = {
-      gofmt.enable = true;
-      golangci-lint.enable = true;
-      golines.enable = true;
-      gotest.enable = true;
-      govet.enable = true;
-    };
-  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
